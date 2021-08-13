42bf3cc7f756eedacc2c152cd91c006b6742aaa6
nacho <ignacioanefra@gmail.com>
2021-08-13 19:08:14 +0200

    First Menu Bar & prettierrc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'myLoL';
+
+  items: MenuItem[] = [];
+  hideMenu: boolean = true;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Options',
+        items: [
+          {
+            label: 'Update',
+            icon: 'pi pi-refresh',
+            command: () => {},
+          },
+          {
+            label: 'Delete',
+            icon: 'pi pi-times',
+            command: () => {},
+          },
+        ],
+      },
+      {
+        label: 'Navigate',
+        items: [
+          {
+            label: 'Angular',
+            icon: 'pi pi-external-link',
+            url: 'http://angular.io',
+          },
+          // {
+          //   label: 'Router',
+          //   icon: 'pi pi-upload',
+          //   routerLink: '/fileupload',
+          // },
+        ],
+      },
+    ];
+  }
 }
